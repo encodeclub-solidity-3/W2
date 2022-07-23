01370c124a79b5780fcd31bbe2c7aa945635c2e7
Christina Polyukh <christinapolyukh@Christinas-MacBook-Air.local>
2022-07-23 18:40:36 -0400

    Updating TODO

--- Project/scripts/castVote.ts
+++ Project/scripts/castVote.ts
@@ -9,7 +9,7 @@ import { CustomBallot } from "../typechain";
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
-// TODO: Make script compatible with token gated voting
+// TODO: Test once ballot contract is deployed
 
 /*
 Goal:
