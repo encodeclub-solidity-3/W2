d052c46389469a271fd4aa7b42a1ae21dfa9ab81
Alice He <alice041189@gmail.com>
2022-07-31 11:06:34 -0700

    apply prettier

--- Project/scripts/mintTokens.ts
+++ Project/scripts/mintTokens.ts
@@ -1,3 +1,4 @@
+/* eslint-disable node/no-missing-import */
 import { Contract, ethers } from "ethers";
 import "dotenv/config";
 import * as tokenJson from "../artifacts/contracts/Token.sol/MyToken.json";
@@ -36,9 +37,7 @@ async function main() {
   // Get address of token from script arguments and create Token object
   if (process.argv.length < 3) throw new Error("Token address missing");
   const tokenAddress = process.argv[2];
-  console.log(
-    `Attaching token contract interface to address ${tokenAddress}`
-  );
+  console.log(`Attaching token contract interface to address ${tokenAddress}`);
   const tokenContract: MyToken = new Contract(
     tokenAddress,
     tokenJson.abi,
@@ -49,18 +48,31 @@ async function main() {
   const recipientAddress = process.argv[3];
   // Log total initial token supply
   const initialTotalSupply = await tokenContract.totalSupply();
-  console.log(`Initial total supply: ${parseFloat(ethers.utils.formatEther(initialTotalSupply))} tokens`);
+  console.log(
+    `Initial total supply: ${parseFloat(
+      ethers.utils.formatEther(initialTotalSupply)
+    )} tokens`
+  );
   // Get token mint quantity of script arguments
   if (process.argv.length < 5) throw new Error("Token mint quantity missing");
   const tokenMintQuantity = parseInt(process.argv[4]);
   console.log(`Token mint quantity: ${tokenMintQuantity}`);
   // Mint tokens
-  const tokenMintTx = await tokenContract.mint(recipientAddress, ethers.utils.parseEther(tokenMintQuantity.toFixed(18)));
+  const tokenMintTx = await tokenContract.mint(
+    recipientAddress,
+    ethers.utils.parseEther(tokenMintQuantity.toFixed(18))
+  );
   await tokenMintTx.wait();
-  console.log(`Tokens successfully minted. Transaction hash: ${tokenMintTx.hash}`);
+  console.log(
+    `Tokens successfully minted. Transaction hash: ${tokenMintTx.hash}`
+  );
   // Log total token supply after mint transaction
   const finalTotalSupply = await tokenContract.totalSupply();
-  console.log(`Final total supply: ${parseFloat(ethers.utils.formatEther(finalTotalSupply))} tokens`);
+  console.log(
+    `Final total supply: ${parseFloat(
+      ethers.utils.formatEther(finalTotalSupply)
+    )} tokens`
+  );
 }
 
 main().catch((error) => {
